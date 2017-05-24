39ac050c2fa832da0e20b092c6f0adfccadd2546
rum-n <manev.ru@gmail.com>
2017-05-24 16:17:26 +0300

    contact form efforts

--- Gemfile
+++ Gemfile
@@ -12,5 +12,10 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
+gem 'sendgrid-ruby'
+gem 'sinatra'
+gem 'pony'
+gem 'rack-test'
+gem 'minitest'
 
  
---
+++ mailer.rb
@@ -0,0 +1,77 @@
+require 'sinatra'
+require 'pony'
+
+post '/' do 
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => "#{name}<#{sender_email}>",
+      :to => 'manev.ru@gmail.com',
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.sendgrid.net', 
+      :port                 => '587',  
+      :user_name            => ENV['app57525483@heroku.com'], 
+      :password             => ENV['oouj9jsb5909'], 
+      :authentication       => :plain, 
+      :enable_starttls_auto => true,
+      :domain               => 'rmanev.com'
+    }    
+  }
+end
+
+
+
+# before do
+#     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+#     headers['Access-Control-Allow-Origin']  = '*'
+#     headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+# end
+
+# # whitelist should be a space separated list of URLs
+# whitelist = ENV['rman.herokuapp.com'].split
+
+# set :protection, :origin_whitelist => whitelist
+
+# Pony.options = {
+#   :via => :smtp,
+#   :via_options => {
+#     :address => 'smtp.sendgrid.net',
+#     :port => '587',
+#     :domain => 'heroku.com',
+#     :user_name => ENV['app57525483@heroku.com'],
+#     :password => ENV['oouj9jsb5909'],
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# }
+
+# post '/' do
+#   email = ""
+#   params.each do |value|
+#     email += "#{value[0]}: #{value[1]}\n"
+#   end
+#   puts email
+#   Pony.mail(
+#     :to => ENV['manev.ru@gmail.com'],
+#     :from => 'noreply@example.com',
+#     :subject => 'New Contact Form',
+#     :body => email
+#   )
+# end
